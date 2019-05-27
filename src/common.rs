84c58791bd8f3f56d94677e20cd5dd4c43971208
Steven Stanfield <stanfield@scarecrowtech.com>
2019-05-27 00:08:23 +0000

    Removed some dead/commented out code.

--- src/common.rs
+++ src/common.rs
@@ -1,29 +1,3 @@
-//use futures::channel::mpsc;
-//use futures::sink::SinkExt;
-
-/*pub fn send_wait<T>(tx: &mut mpsc::Sender<T>, item: T) -> Result<(), &str> {
-    match tx.try_send(item) {
-        Ok(_) => { Ok(()) },
-        Err(in_item) => {
-            let item = in_item.into_inner();
-            await!(tx.clone().send(item));
-            /*match await!(tx.clone().send(item)) {
-                Ok(_tx) => Ok(()),
-                Err(_) => Err("Sending data failed, aborting!"),
-            }*/
-            Ok(())
-        }
-    }
-}*/
-
-/*pub fn send_writer<C: Sync>(writer: &mut C, item: C) -> Result<(), &str> {
-    match writer.try_send(item) {
-        Ok(_) => { Ok(()) },
-        Err(_in_item) => {
-            Err("Tcp sink not ready for data, closing!")
-        },
-    }
-}*/
 
 pub fn last_brace(buf: &[u8]) -> Option<usize> {
     let mut opens = 0;
